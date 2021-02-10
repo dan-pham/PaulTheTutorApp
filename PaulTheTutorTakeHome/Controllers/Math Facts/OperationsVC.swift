7424eccddf03b45ca4b920d875e7ea7ddbd47518
Dan Pham <phamdan.sj@gmail.com>
2021-02-10 15:48:54 -0800

    Add doubles multiplication

--- PaulTheTutorTakeHome/Controllers/Math Facts/OperationsVC.swift
+++ PaulTheTutorTakeHome/Controllers/Math Facts/OperationsVC.swift
@@ -11,9 +11,12 @@ import UIKit
 class OperationsVC: UIViewController {
     
     let questionLabel = PTTitleLabel(textAlignment: .left, fontSize: 20, text: "What shall we do?")
+    let scrollView = PTScrollView(heightConstraint: 540)
+    
     let additionButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "addition")
     let subtractionButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "subtraction")
     let additionSubtractionButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "addition & subtraction")
+    let doublesMultiplicationButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "doubles multiplication")
     let multiplicationButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "multiplication")
     let divisionButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "division")
     let multiplicationDivisionButton = PTButton(titleColor: .white, backgroundColor: Colors.paulDarkGreen, title: "multiplication & division")
@@ -42,6 +45,17 @@ class OperationsVC: UIViewController {
         navigationController?.pushViewController(TypeOfIntegersVC(), animated: true)
     }
     
+    @objc func selectDoublesMultiplication() {
+        parameters.operation = [.multiplication]
+        parameters.integerType = .doubles
+        
+        // For now, just do positive numbers
+        parameters.integerSign = .positive
+        
+        let vc = NumberOfProblemsVC() // PositiveNegativeIntegersVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     @objc func selectMultiplication() {
         parameters.operation = [.multiplication]
         navigationController?.pushViewController(TypeOfIntegersVC(), animated: true)
@@ -59,27 +73,33 @@ class OperationsVC: UIViewController {
     
     private func configureUI() {
         view.backgroundColor = Colors.paulLightGreen
-        view.addSubviews(questionLabel, additionButton, subtractionButton, additionSubtractionButton, multiplicationButton, divisionButton, multiplicationDivisionButton)
+        view.addSubviews(questionLabel, scrollView)
         
         questionLabel.addFlexWidthSetHeightConstraints(to: view)
         
+        scrollView.addSubviews(additionButton, subtractionButton, additionSubtractionButton, doublesMultiplicationButton, multiplicationButton, divisionButton, multiplicationDivisionButton)
+        scrollView.addScrollViewConstraints(to: view, aboveComponent: questionLabel)
+        
         additionButton.addTarget(self, action: #selector(selectAddition), for: .touchUpInside)
-        additionButton.addFlexWidthSetHeightConstraints(to: view, aboveComponent: questionLabel)
+        additionButton.addFlexWidthSetHeightConstraints(to: scrollView, isScrollViewTop: true)
         
         subtractionButton.addTarget(self, action: #selector(selectSubtraction), for: .touchUpInside)
-        subtractionButton.addFlexWidthSetHeightConstraints(to: view, aboveComponent: additionButton)
+        subtractionButton.addFlexWidthSetHeightConstraints(to: scrollView, aboveComponent: additionButton)
         
         additionSubtractionButton.addTarget(self, action: #selector(selectAdditionSubtraction), for: .touchUpInside)
-        additionSubtractionButton.addFlexWidthSetHeightConstraints(to: view, aboveComponent: subtractionButton)
+        additionSubtractionButton.addFlexWidthSetHeightConstraints(to: scrollView, aboveComponent: subtractionButton)
+        
+        doublesMultiplicationButton.addTarget(self, action: #selector(selectDoublesMultiplication), for: .touchUpInside)
+        doublesMultiplicationButton.addFlexWidthSetHeightConstraints(to: scrollView, aboveComponent: additionSubtractionButton)
         
         multiplicationButton.addTarget(self, action: #selector(selectMultiplication), for: .touchUpInside)
-        multiplicationButton.addFlexWidthSetHeightConstraints(to: view, aboveComponent: additionSubtractionButton)
+        multiplicationButton.addFlexWidthSetHeightConstraints(to: scrollView, aboveComponent: doublesMultiplicationButton)
         
         divisionButton.addTarget(self, action: #selector(selectDivision), for: .touchUpInside)
-        divisionButton.addFlexWidthSetHeightConstraints(to: view, aboveComponent: multiplicationButton)
+        divisionButton.addFlexWidthSetHeightConstraints(to: scrollView, aboveComponent: multiplicationButton)
         
         multiplicationDivisionButton.addTarget(self, action: #selector(selectMultiplicationDivistion), for: .touchUpInside)
-        multiplicationDivisionButton.addFlexWidthSetHeightConstraints(to: view, aboveComponent: divisionButton)
+        multiplicationDivisionButton.addFlexWidthSetHeightConstraints(to: scrollView, aboveComponent: divisionButton)
     }
     
 }
